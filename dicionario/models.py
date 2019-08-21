f71caf8ce3ea2b3fea5291d53a0bd889dac37373
Juliano Fischer Naves <julianofischer@gmail.com>
2019-08-21 13:42:14 -0400

    models area, subarea, termo e sugestão

--- dicionario/models.py
+++ dicionario/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from usuarios.models import Usuario
 
-# Create your models here.
+
+class Area(models.Model):
+    area = models.CharField(max_length=100, null=False)
+
+
+class SubArea(models.Model):
+    subarea = models.CharField(max_length=100, null=False)
+    area = models.ForeignKey(Area)
+
+
+class Termo(models.Model):
+    termo = models.CharField(max_length=100, null=False)
+    subarea = models.ForeignKey(SubArea)
+    img = models.ImageField(upload_to="/imagens")
+    expressao = models.CharField(max_length=200, null=False)
+    aprovado = models.BooleanField(default=True)
+
+
+class Sugestao(models.Model):
+    termo = models.ForeignKey(Termo)
+    usuario = models.ForeignKey(Usuario) #o usuário que sugeriu o termo
+    timestamp = models.DateTimeField(auto_now_add=True)
